26b19638c7f193114895835708679722dfcfdd94
Ali Arabi <aliarabibbfftt@gmail.com>
2025-04-27 17:39:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import subprocess
 import sys
+import os
+
+# التحقق من وجود ملف requirements.txt وتثبيت المكتبات المطلوبة
+def install_requirements():
+    if not os.path.isfile('requirements.txt'):
+        print("لم يتم العثور على ملف requirements.txt!")
+        return
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'])
+
+# تثبيت المتطلبات إذا كانت غير مثبتة
+install_requirements()
 
 # التأكد من تثبيت ultralytics قبل استيرادها
 try:
@@ -11,9 +22,7 @@ except ImportError:
 # استيراد المكتبات بعد التأكد من تثبيتها
 import streamlit as st
 from PIL import Image
-from ultralytics import YOLO
 import torch
-import os
 
 # تحميل الموديل المدرب
 model_path = r'C:\GitHub\Streamlit-Yolo-App\yolov8s.pt'  # غير المسار إذا كان موديلك باسم مختلف
